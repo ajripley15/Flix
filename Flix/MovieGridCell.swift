b987457609acb1a35d41e208f70a34c7555f28f9
Amy Ripley <ajripley15@gmail.com>
2019-10-25 14:31:00 -0500

    Part 2 - added navigation and movie grid

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Amy Jo on 10/25/19.
+//  Copyright © 2019 Amy Ripley. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var PosterView: UIImageView!
+    
+}
